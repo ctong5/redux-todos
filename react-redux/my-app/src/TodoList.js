99a595e60adb3cc038c5a0c6ae788b143c861da2
ctong5 <cassandra.tong5@gmail.com>
2019-09-15 15:35:39 -0700

    Connect component to state

--- react-redux/my-app/src/TodoList.js
+++ react-redux/my-app/src/TodoList.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
+import { connect } from 'react-redux';
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      task: '',
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    debugger;
+  }
+
+  handleChange(e) {
+    this.setState ({
+      [e.target.name]: e.target.value
+    });
   }
 
   render() {
-    let todos = this.state.todos.map((task, index) => {
+    let todos = this.props.todos.map((task, index) => {
       return (
         <Todo task={task} key={index} />
       )
@@ -15,12 +32,36 @@ class TodoList extends Component {
 
     return (
       <div>
-        <ul>
-        {todos}
-        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='task'>Task</label>
+          <input 
+            type='text' 
+            name='task' 
+            id='task'
+            onChange={this.handleChange}
+          />
+          <button>Add a Todo!</button>
+        </form>
+        <ul>{todos}</ul>
       </div>
     )
   }
 };
 
-export default TodoList;
+function mapStateToProps(reduxState) {
+  return {
+    todos: reduxState.todos,
+  }
+}
+
+export default connect(mapStateToProps)(TodoList);
+
+/* NOTE: flow of operations...
+* Wrap App in Provider
+* Connect Todolist to store
+* Run createStore function in rootreducer.
+* Retrieve initial state
+* Pass in mapstatetoprops to connect, which is passing in initialstate. Goes to reducer, gets that state, setting as component props
+* componenent renders
+* initially reduxState will be passed in as { todos: Array(0), id: 0 }, turn into props in react component
+*/
