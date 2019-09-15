146184f5d7920ef8c0a91721d5bde904fbd8d2dc
ctong5 <cassandra.tong5@gmail.com>
2019-09-14 22:40:15 -0700

    Correctly dispatch action type ADD_TODO

--- script.js
+++ script.js
@@ -3,15 +3,18 @@
 // set actions so we can store.dispatch
 
 let initialState = {
-  count: 0,
+  todos: [],
+  id: 0,
 }
 
 // function rootReducer to create store
 function rootReducer(state = initialState, action) {
   switch(action.type) {
-    case 'ADD TODO':
+    case 'ADD_TODO':
       // add a todo
-    case 'REMOVE TODO':
+      debugger;
+      return state;
+    case 'REMOVE_TODO':
       // remove a today
     default:
       return state;
@@ -23,5 +26,15 @@ const store = Redux.createStore(rootReducer)
 
 // on DOM load
 $(document).ready(function() {
-
+  // when form is submited, dispatch "ADD_TODO" (send action to reducer). Note you can see action, type in chrome debugger if add debugger in reducer
+  // grab form and trigger reset event to clear form values
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    let newTask = $('#task').val();
+    store.dispatch({
+      type: 'ADD_TODO',
+      task: newTask,
+    });
+    $('form').trigger('reset');
+  })
 })
